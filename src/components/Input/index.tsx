8145101d1b4ef974d0844449d11ad51f5fc30f71
yangwenchao <wenchao.yang1@eeoa.com>
2024-08-12 13:45:37 +0800

    feat: 支持粘贴多个换行的号码

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -3,6 +3,15 @@ import React from 'react';
 function Input(props: { onAdd: (value: string) => void }) {
   const { onAdd } = props;
   const inputRef = React.createRef<HTMLInputElement>();
+  const onPaste = (e: React.ClipboardEvent<HTMLInputElement>) => {
+    const value = e.clipboardData.getData('text');
+    if (!value) return;
+    const values = value.split('\n');
+    values.forEach((v) => {
+      onAdd(v);
+    });
+    e.preventDefault()
+  };
   return (
     <div className=' flex gap-1 rounded-lg ring ring-slate-300 shadow-lg font-medium px-2 py-1 text-lg'>
       <input
@@ -15,6 +24,7 @@ function Input(props: { onAdd: (value: string) => void }) {
             onAdd(inputRef.current!.value);
           }
         }}
+        onPaste={onPaste}
       />
       <button
         className='bg-slate-300 px-2 py-1 rounded-lg w-16'
